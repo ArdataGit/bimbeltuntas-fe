05fee5e380edf10cbc8ec53c90e4e5b61eb54a2c
ArdataGit <ardatapkl@gmail.com>
2025-04-10 04:19:28 +0000

    push from server

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    port: 4175,
+  },
   plugins: [react()],
   resolve: {
     alias: {
